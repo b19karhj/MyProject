fa6ff9f5c70c1271dd5f1219443f111be03a5758
b19karhj <b19karhj@student.his.se>
2021-05-06 14:12:16 +0200

    Added varibals in Auxdata and made to comments more easy to understand

--- app/src/main/java/com/example/myproject/Auxdata.java
+++ app/src/main/java/com/example/myproject/Auxdata.java
@@ -1,10 +1,13 @@
 package com.example.myproject;
 /*
 auxdata:
-wiki
-img
+wiki = url adress to site
+img = url to image
 */
 
 public class Auxdata {
+
+    private String wiki;
+    private String img;
 
 }
