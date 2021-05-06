cf5284a5126d088f8ceb697416ea8bcf78cdd3f5
b19karhj <b19karhj@student.his.se>
2021-05-06 14:03:45 +0200

    Created comment to describe what the different catagories mean for my json data

--- app/src/main/java/com/example/myproject/Building.java
+++ app/src/main/java/com/example/myproject/Building.java
@@ -1,5 +1,22 @@
 package com.example.myproject;
 
+/*
+List for the different type of data
+
+ ID = ID
+ name = name
+ type = login
+ company = country
+ location = city
+ size = height
+ cost = floors
+
+ auxdata:
+ wiki
+ img
+ */
+
+
 public class Building {
 
 }
