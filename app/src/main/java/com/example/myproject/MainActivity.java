70ef63cc58b8c6662d818aa0757001d991a208a1
b19karhj <b19karhj@student.his.se>
2021-05-10 19:28:13 +0200

    Deleted extra spaces

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
 
             Snackbar.make(findViewById(android.R.id.content),buildingArrayList.get(position).info(), Snackbar.LENGTH_LONG).show();
+
             showExternalWebPage(position);
         });
 
