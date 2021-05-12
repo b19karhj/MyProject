8936bfe38bc5fac0be7d1a0f8b0a06f499e8a9b9
b19karhj <b19karhj@student.his.se>
2021-05-12 18:26:57 +0200

    MainActivity.java created missing varibal. activity_main.xml: Added id to link to varibal for textview

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         aboutButton = findViewById(R.id.open);
 
 
-
+        myText = findViewById(R.id.copyRightText);
 
        createMethods();
     }
