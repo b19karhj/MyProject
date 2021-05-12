fd6a1993c7fbd1320ca9da357d792f2e4cadbba1
b19karhj <b19karhj@student.his.se>
2021-05-12 14:46:58 +0200

    Deleted Java class DialogFragment and remade it in correct folder

--- app/src/main/java/com/example/myproject/DialogFragment.java
+++ app/src/main/java/com/example/myproject/DialogFragment.java
@@ -1,3 +1,5 @@
+package com.example.myproject;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -6,11 +8,15 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.myproject.R;
+
 public class DialogFragment extends androidx.fragment.app.DialogFragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
+        inflater.inflate(R.layout.a)
     }
 }
+
