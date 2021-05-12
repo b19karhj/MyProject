b0970472631ef331be280af786cfde869505d288
b19karhj <b19karhj@student.his.se>
2021-05-12 15:51:18 +0200

    Auxdata.java added new string

--- app/src/main/java/com/example/myproject/Auxdata.java
+++ app/src/main/java/com/example/myproject/Auxdata.java
@@ -2,16 +2,19 @@ package com.example.myproject;
 /*
 auxdata:
 wiki =url to image
+CC = Copytirhgt
 
 */
 
 public class Auxdata {
 
     private String wiki;
-
+    private String CC;
 
     public String getWiki() {
         return wiki;
     }
-
+    public String copyRight() {
+        return CC;
+    }
 }
