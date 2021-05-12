200d441cafaa7313a25eb2223f21b323ca93b8ac
b19karhj <b19karhj@student.his.se>
2021-05-12 14:42:22 +0200

    DialogFragment.java: Added on create

--- app/src/main/java/DialogFragment.java
+++ app/src/main/java/DialogFragment.java
@@ -1,5 +1,16 @@
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class DialogFragment extends androidx.fragment.app.DialogFragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
