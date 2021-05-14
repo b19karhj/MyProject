3d6cc4c6ae919cda36c9b9eb744fb09397784b01
b19karhj <b19karhj@student.his.se>
2021-05-14 21:09:51 +0200

    WebViewFragment.java: Added button to close WebViewFragment.java

--- app/src/main/java/com/example/myproject/WebViewFragment.java
+++ app/src/main/java/com/example/myproject/WebViewFragment.java
@@ -1,15 +1,32 @@
 package com.example.myproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class WebViewFragment extends AppCompatActivity{
+
+    private Button close;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.copyright_button);
 
+        close = findViewById(R.id.close_web);
+        closeWeb();
     }
+
+    private void closeWeb() {
+
+        close.setOnClickListener(v -> {
+            finish();
+        });
+
+    }
+
+
 }
